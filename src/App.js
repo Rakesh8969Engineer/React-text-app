b7c1ca3735669790fd06d927c2ee8a9a10cdcc49
Rakesh8969Engineer <socialrakesh1947@gmail.com>
2023-01-19 20:00:05 +0530

    firstreleased

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Nav from "./components/Nav";
+import Textform from './components/Textform'
+let name = "<b> rakesh ranjan</b>";
 
-function App() {
+function App() { 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+  
+    <>
+      <Nav title="Rakesh-Text-App" aboutText="About Us"
+       />
+<div className="container my-3">
+
+<Textform heading="Enter Your Text Here."/>
+
+
+</div>
+    </>
   );
 }
 
---
+++ src/components/Textform.js
@@ -0,0 +1,97 @@
+import React, { useState } from "react";
+
+export default function Textform(props) {
+  const handlueupclick = () => {
+    console.log(" convert uppercase uppercase click succeses"+text);
+    let newtext=text.toUpperCase();
+    settext(newtext);
+  };
+
+const lowercase=()=>{
+let lowtext=text.toLowerCase()
+settext(lowtext);
+
+
+
+
+}
+
+const clearbox=()=>{
+
+  let emptext=""
+  settext( emptext);
+
+}
+
+
+
+
+
+  
+  const handleonchange = (event) => {
+
+
+console.log("hii this is for onchange");
+settext(event.target.value);
+
+  };
+
+
+
+
+
+
+
+
+  const [text, settext] = useState("");
+  // 
+  return (
+    <div>
+      <h1>{props.heading}</h1>
+
+      <div className="form-group">
+        <textarea
+          className="form-control"
+          value={text}
+          onChange={handleonchange}
+          id="mytextbox"
+          rows="10"
+        ></textarea>
+      </div>
+
+      <button className="btn btn-primary  " onClick={handlueupclick}>
+        Convert To Uppercase
+      </button>
+
+
+      <button type="button" className="btn btn-warning  mx-3" onClick={lowercase}   >Convert To LowerCase</button>
+
+
+      <button type="button" className="btn btn-info  mx-2" onClick={clearbox}   >Clear</button>
+<div>
+
+<h5>Total Characters:-{text.length}</h5><br/>
+<p>Total Sentences:-{text.split(" ").length}</p>
+<p>Time to Read:-{text.split("").length*0.008} Seconds</p>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</div>
+
+
+    </div>
+  );
+}
